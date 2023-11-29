5b6754d033864408259981c906b82d6563233159
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-11-29 22:46:39 +0700

    Refactor Docker build

---
+++ build_docker_macos_m1.sh
@@ -0,0 +1,2 @@
+docker build . -f Dockerfile-m1 -t vietanhdev/openvi-image-classification-m1:latest
+docker push vietanhdev/openvi-image-classification-m1:latest
---
+++ train_model_macos_m1.sh
@@ -0,0 +1,12 @@
+docker run -v `pwd`/out_snapshot:/workspace/out_snapshot \
+    -v `pwd`/dataset:/workspace/dataset \
+    vietanhdev/openvi-image-classification-m1:latest \
+    python train.py --network resnet18 \
+    --num_classes 2 \
+    --input_size 224 \
+    --epochs 20 \
+    --learning_rate 0.0005 \
+    --batch_size 64 \
+    --dataset dataset/mvtec \
+    --path_pretrain pretrains/resnet18-5c106cde.pth \
+    --job_name obj_classify
